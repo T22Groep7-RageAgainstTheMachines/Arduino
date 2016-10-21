8e691f50f8c01e650614e24227aac51f30021dea
Mark Steenmans <Marksteenmans@gmail.com>
2016-10-21 09:55:30 +0200

    Updated everything to working version

---
+++ ArduinoSendAndReceiveStringsWifiProftaak/ArduinoSendAndReceiveStringsWifiProftaak.ino
@@ -0,0 +1,103 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "Rp6.h"
+
+int speedLeft;
+int speedRight;
+
+RP6_registers regs;
+RP6_LEDs leds;
+RP6_DIRECTION dir = RP6_FORWARD;
+
+int status = WL_IDLE_STATUS;
+char ssid[] = "ICIDU"; //  your network SSID (name)
+char pass[] = "12345678";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[255]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+WiFiUDP Udp;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid);
+
+    // wait 10 seconds for connection:
+    delay(1000);
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);
+  Serial.println("Connected");
+  Rp6.begin();
+  speedLeft = 0;
+  speedRight = 0;
+
+}
+
+void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Message:");
+    Serial.println(packetBuffer);
+    String message = packetBuffer;
+    processMessage(message);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+}
+
+
+
+
+
+
+
+
---
+++ ArduinoSendAndReceiveStringsWifiProftaak/processMessage.ino
@@ -0,0 +1,43 @@
+void processMessage(String message) {
+  Serial.println(message);
+  if (message == "MF")
+  {
+    Rp6.move(200,  RP6_FORWARD, 1000);
+  }
+  if (message == "MB")
+  {
+    Rp6.move(200,  RP6_BACKWARD, 1000);
+  }
+  if (message == "CD")
+  {
+    Rp6.stop();
+    if (dir == RP6_BACKWARD)
+    {
+      dir = RP6_FORWARD;
+    }
+    else
+    {
+      dir = RP6_BACKWARD;
+    }
+  }
+  if (message == "STOP")
+  {
+    Rp6.stop();
+  }
+  if (message == "RR")
+  {
+    Rp6.rotate(200, RP6_RIGHT, 180);
+  }
+  if (message == "RL")
+  {
+    Rp6.rotate(200, RP6_LEFT, 180);
+  }
+
+  else
+  {
+    return;
+  }
+}
+
+
+
