75f692bb9c4a0a5224ff5f183a7b99d8d0e9490b
Mark Steenmans <Marksteenmans@gmail.com>
2016-11-18 14:25:40 +0100

    Updated protocol to function with maghdioag4

--- ArduinoSendAndReceiveStringsWifiProftaak/processMessage.ino
+++ ArduinoSendAndReceiveStringsWifiProftaak/processMessage.ino
@@ -1,14 +1,14 @@
 void processMessage(String message) {
   Serial.println(message);
-  if (message == "MF")
+  if (message == "%MF#")
   {
     Rp6.move(200,  RP6_FORWARD, 1000);
   }
-  if (message == "MB")
+  if (message == "%MB#")
   {
     Rp6.move(200,  RP6_BACKWARD, 1000);
   }
-  if (message == "CD")
+  if (message == "%CD#")
   {
     Rp6.stop();
     if (dir == RP6_BACKWARD)
@@ -20,15 +20,15 @@ void processMessage(String message) {
       dir = RP6_BACKWARD;
     }
   }
-  if (message == "STOP")
+  if (message == "%STOP#")
   {
     Rp6.stop();
   }
-  if (message == "RR")
+  if (message == "%RR#")
   {
     Rp6.rotate(200, RP6_RIGHT, 180);
   }
-  if (message == "RL")
+  if (message == "#RL%")
   {
     Rp6.rotate(200, RP6_LEFT, 180);
   }
